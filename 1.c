2bae360a7c87beee2fcb323fb1eb7b24625cf2f4
Brian Chrzanowski <chrzanowskitest@gmail.com>
2023-09-14 01:43:05 -0400

    everything looks good on #1

--- 1.c
+++ 1.c
@@ -3,78 +3,221 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <assert.h>
 #include <jansson.h>
 
 #define COMMON_IMPLEMENTATION
 #include "common.h"
 
-typedef struct {
-    char *src;
-    char *dest;
-    struct {
-        char *type;
-        int64_t msg_id;
-        char *echo;
-    } body;
-} EchoRequest;
+typedef enum {
+    MESSAGE_TYPE_NONE,
+    MESSAGE_TYPE_ERROR,
+    MESSAGE_TYPE_INIT,
+    MESSAGE_TYPE_INIT_OK,
+    MESSAGE_TYPE_ECHO,
+    MESSAGE_TYPE_ECHO_OK,
+    MESSAGE_TYPE_TOTAL,
+} MESSAGE_TYPE;
+
+char *MESSAGE_TYPE_TABLE[] = {
+    NULL,
+    "error",
+    "init",
+    "init_ok",
+    "echo",
+    "echo_ok",
+    NULL
+};
+
+typedef enum {
+    ERROR_TIMEOUT = 0,
+    ERROR_NODENOTFOUND = 1,
+    ERROR_NOTSUPPORTED = 10,
+    ERROR_TEMPORARILYUNAVAILABLE = 11,
+    ERROR_MALFORMEDREQUEST = 12,
+    ERROR_CRASH = 13,
+    ERROR_ABORT = 14,
+    ERROR_KEYDOESNOTEXIST = 20,
+    ERROR_KEYALREADYEXISTS = 21,
+    ERROR_PRECONDITIONFAILED = 22,
+    ERROR_TXNCONFLICT = 30,
+} ERRORS;
 
 typedef struct {
     char *src;
     char *dest;
-    struct {
-        char *type;
-        int64_t msg_id;
-        int64_t in_reply_to;
-        char *echo;
-    } body;
-} EchoResponse;
 
-// serializes the message to the spec here: https://fly.io/dist-sys/1/
-EchoRequest deserialize_message(char *json)
+    MESSAGE_TYPE type;
+    int64_t id;
+    int64_t msg_id;
+    int64_t in_reply_to;
+
+    union {
+        struct {
+            char *node_id;
+            char **node_ids; // pointer list (last element is NULL, be careful)
+        } init;
+
+        struct {
+        } init_ok;
+
+        struct {
+            int64_t code;
+        } error;
+
+        // KEEP THE LAYOUT OF THE ECHO AND ECHO_OK STRUCTURES THE SAME
+        struct {
+            char *echo;
+        } echo;
+
+        struct {
+            char *echo;
+        } echo_ok;
+    } fields;
+} SystemMessage;
+
+static char *node_id = NULL;
+static int64_t msg_id = 0;
+
+MESSAGE_TYPE message_type_from_str(char *s)
 {
-    EchoRequest m = {0};
+    for (size_t i = 0; i < ARRSIZE(MESSAGE_TYPE_TABLE); i++) {
+        if (MESSAGE_TYPE_TABLE[i] && streq(s, MESSAGE_TYPE_TABLE[i])) {
+            return i;
+        }
+    }
+    return MESSAGE_TYPE_NONE;
+}
 
-    json_t *root;
+char *str_from_message_type(MESSAGE_TYPE type)
+{
+    return MESSAGE_TYPE_TABLE[type];
+}
+
+int64_t next_message_id()
+{
+    return ++msg_id;
+}
+
+// Parses the input message into a SystemMessage, and returns it. NULL on error. (We don't error).
+SystemMessage *deserialize(char *json)
+{
+    SystemMessage *m = calloc(1, sizeof(*m));
+
+    json_t *root, *body;
     json_error_t error;
 
     root = json_loads(json, 0, &error);
-    m.src = strdup(json_string_value(json_object_get(root, "src")));
-    m.dest = strdup(json_string_value(json_object_get(root, "dest")));
-    json_t *body = json_object_get(root, "body");
-    m.body.type = strdup(json_string_value(json_object_get(body, "type")));
-    m.body.msg_id = (int64_t)json_number_value(json_object_get(body, "msg_id"));
-    m.body.echo = strdup(json_string_value(json_object_get(body, "echo")));
+    body = json_object_get(root, "body");
+
+    m->src = strdup(json_string_value(json_object_get(root, "src")));
+    m->dest = strdup(json_string_value(json_object_get(root, "dest")));
+    m->type = message_type_from_str((char *)json_string_value(json_object_get(body, "type")));
+    m->id = (int64_t)json_number_value(json_object_get(body, "id"));
+    m->msg_id = (int64_t)json_number_value(json_object_get(body, "msg_id"));
+    m->in_reply_to = (int64_t)json_number_value(json_object_get(body, "in_reply_to"));
+
+    switch (m->type) {
+        case MESSAGE_TYPE_INIT: {
+            m->fields.init.node_id = strdup(json_string_value(json_object_get(body, "node_id")));
+
+            json_t *arr = json_object_get(body, "node_id");
+            m->fields.init.node_ids = calloc(json_array_size(arr) + 1, sizeof(*m->fields.init.node_ids));
+
+            size_t index;
+            json_t *value;
+            json_array_foreach(arr, index, value) {
+                m->fields.init.node_ids[index] = strdup(json_string_value(value));
+            }
+            break;
+        }
+        case MESSAGE_TYPE_INIT_OK:
+            break;
+        case MESSAGE_TYPE_ECHO:
+            m->fields.echo.echo = strdup(json_string_value(json_object_get(body, "echo")));
+            break;
+        case MESSAGE_TYPE_ECHO_OK:
+            m->fields.echo_ok.echo = strdup(json_string_value(json_object_get(body, "echo")));
+            break;
+        default:
+            fprintf(stderr, "Message type: '%s' (%d) not handled!\n", MESSAGE_TYPE_TABLE[m->type], m->type);
+            assert(0);
+    }
 
     json_decref(root);
 
     return m;
 }
 
-EchoResponse create_response_for_request(EchoRequest *request)
+SystemMessage *create_response(SystemMessage *request)
 {
-    EchoResponse response = {0};
+    SystemMessage *response = calloc(1, sizeof(*response));
 
-    response.src = request->dest;
-    response.dest = request->src;
-    response.body.type = "echo_ok";
-    response.body.msg_id = request->body.msg_id;
-    response.body.in_reply_to = request->body.msg_id;
-    response.body.echo = request->body.echo;
+    response->type = request->type;
+
+    switch (response->type) {
+        case MESSAGE_TYPE_INIT:
+            response->type = MESSAGE_TYPE_INIT_OK;
+            node_id = strdup(request->fields.init.node_id);
+            break;
+        case MESSAGE_TYPE_INIT_OK:
+            break;
+        case MESSAGE_TYPE_ECHO:
+            response->type = MESSAGE_TYPE_ECHO_OK;
+            response->fields.echo_ok.echo = strdup(request->fields.echo.echo);
+            break;
+        case MESSAGE_TYPE_ECHO_OK:
+            break;
+        default:
+            fprintf(stderr, "Message type: '%s' (%d) not handled!\n",
+                MESSAGE_TYPE_TABLE[response->type], response->type);
+            assert(0);
+    }
+
+    response->src = strdup(node_id);
+    response->dest = strdup(request->src);
+
+    response->id = request->id;
+    response->msg_id = next_message_id();
+    response->in_reply_to = request->msg_id;
 
     return response;
 }
 
-// deserializes the message to the spec here: https://fly.io/dist-sys/1/
-char *serialize_message(EchoResponse *response)
+char *serialize(SystemMessage *m)
 {
     json_t *root = json_object();
-    json_object_set_new(root, "src", json_string(response->src));
-    json_object_set_new(root, "dest", json_string(response->dest));
+    json_object_set_new(root, "src", json_string(m->src));
+    json_object_set_new(root, "dest", json_string(m->dest));
+    json_object_set_new(root, "id", json_integer(m->id));
+
     json_t *body = json_object();
-    json_object_set_new(body, "type", json_string(response->body.type));
-    json_object_set_new(body, "msg_id", json_integer(response->body.msg_id));
-    json_object_set_new(body, "in_reply_to", json_integer(response->body.in_reply_to));
-    json_object_set_new(body, "echo", json_string(response->body.echo));
+    json_object_set_new(body, "type", json_string(str_from_message_type(m->type)));
+    json_object_set_new(body, "in_reply_to", json_integer(m->in_reply_to));
+
+    if (m->type != MESSAGE_TYPE_INIT && m->type != MESSAGE_TYPE_INIT_OK)
+        json_object_set_new(body, "msg_id", json_integer(m->msg_id));
+
+    switch (m->type) {
+        case MESSAGE_TYPE_INIT:
+            // TODO
+            assert(0);
+            break;
+        case MESSAGE_TYPE_INIT_OK:
+            break;
+        case MESSAGE_TYPE_ECHO:
+            json_object_set_new(body, "echo", json_string(m->fields.echo.echo));
+            break;
+        case MESSAGE_TYPE_ECHO_OK:
+            json_object_set_new(body, "echo", json_string(m->fields.echo_ok.echo));
+            break;
+        default:
+            fprintf(stderr, "Message type: '%s' (%d) not handled!\n",
+                MESSAGE_TYPE_TABLE[m->type], m->type);
+            assert(0);
+    }
+
+
     json_object_set_new(root, "body", body);
 
     char *output = json_dumps(root, JSON_COMPACT);
@@ -84,12 +227,31 @@ char *serialize_message(EchoResponse *response)
     return output;
 }
 
-void release_echo_request(EchoRequest *request)
+void free_message(SystemMessage *m)
 {
-    free(request->src);
-    free(request->dest);
-    free(request->body.echo);
-    free(request->body.type);
+    free(m->src);
+    free(m->dest);
+
+    switch (m->type) {
+        case MESSAGE_TYPE_INIT:
+            free(m->fields.init.node_id);
+            for (size_t i = 0; m->fields.init.node_ids[i]; i++)
+                free(m->fields.init.node_ids[i]);
+            break;
+        case MESSAGE_TYPE_INIT_OK:
+            break;
+        case MESSAGE_TYPE_ECHO_OK:
+            free(m->fields.echo_ok.echo);
+            break;
+        case MESSAGE_TYPE_ECHO:
+            free(m->fields.echo.echo);
+            break;
+        default:
+            fprintf(stderr, "Message type: '%s' (%d) not handled!\n", MESSAGE_TYPE_TABLE[m->type], m->type);
+            assert(0);
+    }
+
+    free(m);
 }
 
 int main(int argc, char **argv)
@@ -101,17 +263,25 @@ int main(int argc, char **argv)
     while ((nread = getline(&line, &len, stdin)) != -1) {
         char *tline = trim(line);
 
-        EchoRequest request = deserialize_message(tline);
-        EchoResponse response = create_response_for_request(&request);
-        char *output_json = serialize_message(&response);
+        fprintf(stderr, "I: %s\n", tline);
 
-        printf("%s\n", output_json);
+        SystemMessage *request = deserialize(tline);
+        SystemMessage *response = create_response(request);
+        char *json = serialize(response);
 
-        free(output_json);
-        release_echo_request(&request);
+        fprintf(stderr, "O: %s\n", json);
+        printf("%s\n", json);
+
+        fflush(stdout);
+        fflush(stderr);
+
+        free_message(request);
+        free_message(response);
+        free(json);
     }
 
     free(line);
+    free(node_id);
 
     return 0;
 }
